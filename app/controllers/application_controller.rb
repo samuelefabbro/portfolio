246eb9bf1f92f3a079071bfed91b5c8485902e0b
samuelefabbro <samuele.fabbro@gmail.com>
2018-04-25 01:10:08 +0200

    Added dynamic categories in the menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :all_categories
+
+  def all_categories
+    @categories = Category.all
+  end
 end
